31e2a57b15a9a602727b22582c4b72d59617b75b
linilinn <125601952+linilinn@users.noreply.github.com>
2023-04-01 10:51:14 +0300

    add button transaction

--- app/src/main/java/com/example/fourth/MainActivity.java
+++ app/src/main/java/com/example/fourth/MainActivity.java
@@ -1,23 +1,49 @@
 package com.example.fourth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.fourth.fragments.FirstFragment;
+import com.example.fourth.fragments.SecondFragment;
+import com.example.fourth.fragments.ThirdFragment;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
+    Fragment firstFragment, secondFragment, thirdFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+        thirdFragment = new ThirdFragment();
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.main_fragment, FirstFragment.class, null)
-                    .commit();
+            fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().setReorderingAllowed(true).add(R.id.main_fragment, FirstFragment.class, null).commit();
         }
+    }
+
+    public void changeFragment(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (view.getId() == R.id.button) {
+            if (secondFragment.isVisible()) {
+                return;
+            }
+            fragmentTransaction.replace(R.id.main_fragment, secondFragment);
+        }
+        if (view.getId() == R.id.button2) {
+            if (thirdFragment.isVisible()) {
+                return;
+            }
+            fragmentTransaction.replace(R.id.main_fragment, thirdFragment);
+        }
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/fourth/fragments/FirstFragment.java
+++ app/src/main/java/com/example/fourth/fragments/FirstFragment.java
@@ -1,66 +1,51 @@
 package com.example.fourth.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.fourth.MainActivity;
 import com.example.fourth.R;
+import com.example.fourth.databinding.FragmentFirstBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FirstFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    FragmentManager fragmentManager;
+    FragmentFirstBinding binding;
+    MainActivity mainActivity;
     public FirstFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(R.layout.fragment_first);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+    public void onViewCreated(@NonNull View view1, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view1, savedInstanceState);
+        binding.button.setOnClickListener(view -> mainActivity.changeFragment(view));
+        binding.button2.setOnClickListener(view -> mainActivity.changeFragment(view));
     }
 }
