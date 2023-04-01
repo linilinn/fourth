2ccd5d093aaceca8d4f3505f24ba00853017e18d
linilinn <125601952+linilinn@users.noreply.github.com>
2023-04-01 16:28:18 +0300

    recyclerView initialized

--- app/src/main/java/com/example/fourth/adapter/Food.java
+++ app/src/main/java/com/example/fourth/adapter/Food.java
@@ -4,12 +4,10 @@ public class Food {
     private String food;
     private int imageResource;
 
-    public Food() {
-        this("Food");
-    }
 
-    public Food(String food) {
+    public Food(String food, int imageResource) {
         this.food = food;
+        this.imageResource = imageResource;
     }
 
     public String getFood() {
