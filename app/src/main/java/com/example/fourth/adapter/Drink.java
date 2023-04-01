32c87691ef1fcbba0872d53d3b28a24a88111bc5
linilinn <125601952+linilinn@users.noreply.github.com>
2023-04-01 12:18:50 +0300

    add classes Drink, Food

---
+++ app/src/main/java/com/example/fourth/adapter/Drink.java
@@ -0,0 +1,17 @@
+package com.example.fourth.adapter;
+
+public class Drink {
+    private String drink;
+
+    public Drink() {
+        this("Drink");
+    }
+
+    public Drink(String drink) {
+        this.drink = drink;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+}
---
+++ app/src/main/java/com/example/fourth/adapter/Food.java
@@ -0,0 +1,17 @@
+package com.example.fourth.adapter;
+
+public class Food {
+    private String food;
+
+    public Food() {
+        this("Food");
+    }
+
+    public Food(String food) {
+        this.food = food;
+    }
+
+    public String getFood() {
+        return food;
+    }
+}
